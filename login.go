ed79aedaac9e1b79c81b342b5823bdff74c80e85
ryan2156 <64963540+ryan2156@users.noreply.github.com>
2024-08-15 14:49:31 +0800

    sql connection prototype

--- login.go
+++ login.go
@@ -2,38 +2,79 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"net/http"
 
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var UserData map[string]string
 
+type User struct {
+	user_id  string
+	password string
+}
+
+var DB *sql.DB
+
+func InitDB(dataSourceName string) {
+	var err error
+	DB, err = sql.Open("sqlite3", dataSourceName)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// 确保连接是可用的
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+}
+
 func init() {
-	UserData = map[string]string{
-		"test": "test",
+	// UserData = map[string]string{
+	// 	"test": "test",
+	// }
+	type User struct {
+		user_id  string
+		password string
 	}
 }
 
-func CheckUserIsExist(username string) bool {
-	_, isExist := UserData[username]
-	return isExist
-}
-
-func CheckPassword(p1 string, p2 string) error {
-	if p1 == p2 {
-		return nil
-	} else {
-		return errors.New("password is not correct")
+// CheckUserExists checks if a user exists in the database
+func CheckUserExists(user_id string) (bool, error) {
+	var user User
+	err := DB.QueryRow("SELECT user_id, password FROM Users WHERE user_id = ?", user_id).Scan(&user.user_id, &user.password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
 	}
+	return true, nil
 }
 
+// Auth authenticates a user by username and password
 func Auth(username string, password string) error {
-	if isExist := CheckUserIsExist(username); isExist {
-		return CheckPassword(UserData[username], password)
-	} else {
-		return errors.New("user is not exist")
+	var user User
+	err := DB.QueryRow("SELECT user_id, password FROM Users WHERE user_id = ?", username).Scan(&user.user_id, &user.password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return errors.New("user does not exist")
+		}
+		return err
 	}
+
+	if user.password != password {
+		return errors.New("password is incorrect")
+	}
+
+	return nil
 }
 
 func LoginPage(c *gin.Context) {
@@ -42,11 +83,11 @@ func LoginPage(c *gin.Context) {
 
 func LoginAuth(c *gin.Context) {
 	var (
-		username string
+		user_id  string
 		password string
 	)
-	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
-		username = in
+	if in, isExist := c.GetPostForm("user_id"); isExist && in != "" {
+		user_id = in
 	} else {
 		c.HTML(http.StatusBadRequest, "login.html", gin.H{
 			"error": errors.New("必須輸入使用者名稱"),
@@ -61,7 +102,7 @@ func LoginAuth(c *gin.Context) {
 		})
 		return
 	}
-	if err := Auth(username, password); err == nil {
+	if err := Auth(user_id, password); err == nil {
 		c.HTML(http.StatusOK, "login.html", gin.H{
 			"success": "登入成功",
 		})
@@ -75,6 +116,12 @@ func LoginAuth(c *gin.Context) {
 }
 
 func main() {
+
+	InitDB("./database.db")
+	defer DB.Close()
+
+	fmt.Println("Data inserted successfully!")
+
 	server := gin.Default()
 	server.LoadHTMLGlob("./login.html")
 	//設定靜態資源的讀取)
