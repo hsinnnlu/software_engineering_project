b8c2a370f59455d8e6e4266cf1792c5b6561eb8f
lucyhsieh <99053syes@gmail.com>
2024-10-19 11:25:48 +0800

    簽到介面整合

--- webpage/js/Check_in.js
+++ webpage/js/Check_in.js
@@ -1,2 +1,18 @@
-$(".Record").load("Record.html");
+//簽到退切換
+let ischeckin = true;
+document.getElementById("Switch_mode").addEventListener('click', function(){
+    const switchbtn = document.getElementById("Switch_mode");
+    const submitbtn = document.getElementById("Check_in_Btn");
 
+    if(ischeckin){
+        switchbtn.innerHTML = "切換為簽到";
+        submitbtn.innerHTML = "簽退";
+        ischeckin = false;
+        alert("切換為簽退");
+    }else{
+        switchbtn.innerHTML = "切換為簽退";
+        submitbtn.innerHTML = "簽到";
+        ischeckin = true;
+        alert("切換為簽到");
+    }
+})
